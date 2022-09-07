bbe19b8bdda799c90c1a967a37b141c917546814
Zafar Iqbal Khan <zkhan@psu.edu.sa>
2022-09-07 17:32:08 +0300

    Pushing JenkinsWithGit.java

---
+++ JenkinsWithGit.java
@@ -0,0 +1,18 @@
+package miscelleneous;
+
+/**
+ *
+ * @author Zafar Iqbal Khan, zkhan@psu.edu.sa
+ */
+public class JenkinsWithGit {
+    public static void main(String[] args) {
+        //code to print Unicode characters
+        int charPerLine=0;
+        for (int i = 33; i < 256; i++, charPerLine++) 
+        {
+            System.out.print((char)i+((charPerLine%10==0)?"\n":"\t")); 
+            System.out.print();
+        }
+    }
+
+}
