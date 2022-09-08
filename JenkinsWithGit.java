6f672965781035f8338f210308bc058676cfa2f1
Zafar Iqbal Khan <zkhan@psu.edu.sa>
2022-09-08 19:48:27 +0300

    After adding Banner

--- JenkinsWithGit.java
+++ JenkinsWithGit.java
@@ -6,8 +6,9 @@ package miscelleneous;
  */
 public class JenkinsWithGit {
     public static void main(String[] args) {
-        //code to print Unicode characters
+        //code to print ASCII characters
         int charPerLine=0;
+			System.out.println("Printing ASCII Characters");
         for (int i = 33; i < 256; i++, charPerLine++) 
         {
             System.out.print((char)i+((charPerLine%10==0)?"\n":"\t")); 
